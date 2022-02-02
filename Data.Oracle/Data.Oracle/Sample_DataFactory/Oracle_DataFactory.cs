78fac70ed4933e014bb6e2cbaa35fc0029d5a20d
kolinlocke <kolinlocke@gmail.com>
2022-02-02 22:44:01 +0800

    Added Sample Oracle DataFactory

---
+++ Data.Oracle/Data.Oracle/Sample_DataFactory/Oracle_DataFactory.cs
@@ -0,0 +1,35 @@
+﻿using Data.DataFactory;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Data.DataFactory.DataFactory;
+
+namespace Data.Oracle.Sample_DataFactory
+{
+    public class Oracle_DataFactory : DataFactoryBase
+    {
+        String mConnectionData;
+
+        public Oracle_DataFactory(String ConnectionData)
+        {
+            this.mConnectionData = ConnectionData;
+        }
+
+        public override DataFactory.DataFactory.SetupParams Setup_Params()
+        {
+            SetupParams DataFactory_SetupParams = new SetupParams();
+            DataFactory_SetupParams.FactoryName = "DataFactory_Fits";
+            //DataFactory_SetupParams.AssemblyFilePath = ConfigurationManager.AppSettings["DataFactory_AssemblyFilePath"];
+            DataFactory_SetupParams.AssemblySourceType = typeof(Data.Oracle.Implementations.Setup);
+            DataFactory_SetupParams.ClassName_Setup = "Implementations.Setup";
+            DataFactory_SetupParams.ClassName_EntityRepository = "Implementations.EntityRepository";
+            DataFactory_SetupParams.ClassName_SystemParamRepository = "Implementations.SystemParamRepository";
+            DataFactory_SetupParams.ConnectionData = this.mConnectionData;
+
+            return DataFactory_SetupParams;
+        }
+    }
+}
